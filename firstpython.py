1ebae9702171a392a522fb9e1622beea36202fda
niraj1393 <69768819+niraj1393@users.noreply.github.com>
2020-08-16 20:04:43 +0000

    My first program on Github
    Print two numbers.

---
+++ firstpython.py
@@ -0,0 +1,9 @@
+#My Jupyter Notebook on IBM Watson Studio
+
+#Niraj Patel
+
+#I am jnterested in Data Science because it is an umbrella set of mathematics, programming and soft skills.
+
+#The code is to print two numbers
+
+Print("The two numbers are 1\n 2")
