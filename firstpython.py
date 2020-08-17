2c3eab6f8fc87269481fba498cbb586331e9cec0
niraj1393 <69768819+niraj1393@users.noreply.github.com>
2020-08-17 00:59:18 +0000

    Delete firstpython.py

--- firstpython.py
+++
@@ -1,9 +0,0 @@
-#My Jupyter Notebook on IBM Watson Studio
-
-#Niraj Patel
-
-#I am jnterested in Data Science because it is an umbrella set of mathematics, programming and soft skills.
-
-#The code is to print two numbers
-
-Print("The two numbers are 1\n 2")
